9f7efea186c5d73bbb61ad37a5d509fb19298258
JulioAraiza <88641297+JulioAraiza@users.noreply.github.com>
2021-08-16 18:52:28 -0500

    Update exercise.py

--- 04AreaCirculo/src/exercise.py
+++ 04AreaCirculo/src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-
-if __name__ == '__main__':
+import math
+radio = 5
+pi = math.pi
+area = ((radio * radio)) * pi
+print("El area de un círculo de radio 5 es: ")
+print(area)
+if __name__ == '__mainimport math
     main()
