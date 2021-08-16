e0d24eeb33a41050fdceb198cd9d8d2846ef1151
JulioAraiza <88641297+JulioAraiza@users.noreply.github.com>
2021-08-16 18:54:34 -0500

    Solucion

--- 06Vendedor/src/exercise.py
+++ 06Vendedor/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+venta_1 = 500
+venta_2 = 750
+venta_3 = 450
+total_ventas = venta_1 + venta_2 + venta_3
+comision_agente = total_ventas * 0.1
 
+print("La comisión del mes de agosto es: ")
+print(float(comision_agente))
 if __name__ == '__main__':
     main()
