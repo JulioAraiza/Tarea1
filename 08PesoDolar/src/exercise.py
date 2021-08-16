f8a2ec12592a2aaf6b31a196f3461d222ddcafbb
JulioAraiza <88641297+JulioAraiza@users.noreply.github.com>
2021-08-16 18:58:52 -0500

    Solucion

--- 08PesoDolar/src/exercise.py
+++ 08PesoDolar/src/exercise.py
@@ -1,5 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
+cant_pesos = float(25468)
+cant_dolares = float(cant_pesos / 20)
+print("El dolar esta a 20 pesos, por lo que 25468 pesos son: ")
+print(f"{cant_dolares} dolares")
 
 if __name__ == '__main__':
     main()
