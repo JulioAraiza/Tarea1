c26713dec4cb630a52337650e8748832ee30e65b
JulioAraiza <88641297+JulioAraiza@users.noreply.github.com>
2021-08-16 19:05:00 -0500

    Solucion

--- 10DiscoDuro/src/exercise.py
+++ 10DiscoDuro/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-
+gB = 64
+mB = gB * 1024
+kB = mB * 1024
+byte = kB * 1024
+print("En un disco duro de 64 gigas hay: ")
+print(f"{mB} megabytes")
+print(f"{kB} kilobytes")
+print(f"{byte} bytes")
 if __name__ == '__main__':
     main()
